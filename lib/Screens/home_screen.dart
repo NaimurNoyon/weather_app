c9852f503186bed41c58079faf0b922a87f3f6b0
Naimur Noyon <57313497+NaimurNoyon@users.noreply.github.com>
2024-02-08 17:26:47 +0600

    Initial commit

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,210 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weather_app/Utils/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.colorBackground,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                color: AppColors.colorPrimary,
+                size: 22.sp,
+              ),
+              Text(
+                "Rajshahi, Bangladesh",
+                style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 8.h),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(
+                  color: AppColors.colorPrimary,
+                  spreadRadius: -2.0,
+                  blurRadius: 10,
+                  offset: Offset(0, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Text("Sunday, 8 March 2024"),
+            ),
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          slider(),
+          SizedBox(
+            height: 20.h,
+          ),
+          Expanded(
+            child: Container(
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    AppColors.colorBackground,
+                    AppColors.colorPrimaryLightest,
+                  ],
+                  // Three colors: blue, green, and red
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  tileMode: TileMode.clamp,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    height: 100.h,
+                    width: double.maxFinite,
+                    margin: EdgeInsets.symmetric(horizontal: 30.w),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: AppColors.colorPrimary,
+                          spreadRadius: -2.0,
+                          blurRadius: 40,
+                          offset: Offset(0, 1), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 50.h,
+                          width: 100.w,
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  //CarouselSlider Widget
+  Widget slider() {
+    return CarouselSlider(
+      items: [
+        Container(
+          height: 250,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            color: AppColors.colorPrimary,
+            gradient: LinearGradient(
+              colors: [
+                AppColors.colorPrimaryLightest,
+                AppColors.colorPrimaryLighter,
+                AppColors.colorPrimaryLightest,
+              ],
+              // Three colors: blue, green, and red
+              begin: Alignment.topCenter,
+              end: Alignment.bottomLeft,
+              stops: [0.1, 0.4, 0.8],
+              // Adjust stops for a smooth transition
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        Container(
+          height: 250,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            color: AppColors.colorPrimary,
+            gradient: LinearGradient(
+              colors: [
+                AppColors.colorPrimaryLightest,
+                AppColors.colorPrimaryLighter,
+                AppColors.colorPrimaryLighter,
+                AppColors.colorPrimaryLightest,
+              ],
+              // Three colors: blue, green, and red
+              begin: Alignment.topCenter,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 0.3, 0.7, 0.9],
+              // Adjust stops for a smooth transition
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        Container(
+          height: 250,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            color: AppColors.colorPrimary,
+            gradient: LinearGradient(
+              colors: [
+                AppColors.colorPrimaryLightest,
+                AppColors.colorPrimaryLighter,
+                AppColors.colorPrimaryLighter,
+                AppColors.colorPrimaryLightest,
+              ],
+              // Three colors: blue, green, and red
+              begin: Alignment.topCenter,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 0.3, 0.7, 0.9],
+              // Adjust stops for a smooth transition
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+      ],
+      options: CarouselOptions(
+          height: 250.h,
+          enlargeCenterPage: true,
+          autoPlay: false,
+          aspectRatio: 9 / 16,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enableInfiniteScroll: true,
+          autoPlayAnimationDuration: const Duration(milliseconds: 800),
+          viewportFraction: 0.65,
+          onPageChanged: (index, reason) {
+            /*setState(() {
+              _currentPage = index;
+            });*/
+          }),
+    );
+  }
+}
---
+++ lib/Utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const colorPrimary = Color(0xff5143B1);
+  static const colorPrimaryLight = Color(0xff6d62b6);
+  static const colorPrimaryLighter = Color(0xff7f76b0);
+  static const colorPrimaryLightest = Color(0xffb8b2e5);
+  static const colorPrimaryDark = Color(0xff4839af);
+  static const colorPrimaryDarkest = Color(0xff392b98);
+
+  static const colorBackground = Color(0xffF1F0FA);
+
+}
